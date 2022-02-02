64bce679e85269ab9bc8833d075296eb9b78da01
victorpmessias <victor.p.messias@gmail.com>
2022-02-02 19:20:32 -0300

    Adicionado BoardException

--- src/main/java/boardgame/Board.java
+++ src/main/java/boardgame/Board.java
@@ -1,30 +1,57 @@
 package boardgame;
 
 import lombok.Getter;
-import lombok.Setter;
 
 public class Board {
-  @Getter @Setter private int rows;
-  @Getter @Setter int columns;
+  @Getter
+  private int rows;
+  @Getter
+  int columns;
   private Piece[][] pieces;
 
   public Board(int rows, int columns) {
+    if (rows < 1 || columns < 1){
+      throw new BoardException("Erro criando tabuleiro é necessário que haja pelo menos 1 linha e 1 coluna.");
+    }
     this.rows = rows;
     this.columns = columns;
     pieces = new Piece[rows][columns];
-  }	
-  
+  }
+
   public Piece piece(int row, int column) {
+    if(!positionExists(row, column)){
+      throw new BoardException("Posição inexistente.");
+    }
     return pieces[row][column];
   }
 
-  public Piece piece(Position position){
+  public Piece piece(Position position) {
+    if(!positionExists(position)){
+      throw new BoardException("Posição inexistente.");
+    }
     return pieces[position.getRow()][position.getColumn()];
   }
 
-public void placePiece( Piece piece, Position position){
-  pieces[position.getRow()][position.getColumn()] = piece;
-  piece.position = position;
-}
+  public void placePiece(Piece piece, Position position) {
+    if (thereIsAPiece(position)){
+      throw new BoardException("Ja tem uma peça na posição "+position);
+    }
+    pieces[position.getRow()][position.getColumn()] = piece;
+    piece.position = position;
+  }
 
+  private boolean positionExists(int row, int column) {
+    return row >= 0 && row < rows && column >= 0 && column < columns;
+  }
+
+  public boolean positionExists(Position position) {
+    return positionExists(position.getRow(), position.getColumn());
+  }
+
+  public boolean thereIsAPiece(Position position){
+    if(!positionExists(position)){
+      throw new BoardException("Posição inexistente.");
+    }
+    return piece(position) != null;
+  }
 }
